42b4ba18a503cfa839c9dbb909c7c92588b32437
PhapKhuong <phapkhuong@gmail.com>
2022-11-21 19:09:07 +0700

    Sửa dấu == thành ===; bỏ dấu ngoặc nhọn khi có 1 dòng lệnh

--- ChuyenDoiTienTe.js
+++ ChuyenDoiTienTe.js
@@ -13,23 +13,23 @@ function doSth()
         let fromCurrency = document.getElementById('fCurrency').value;
         let toCurrency = document.getElementById('tCurrency').value;
         let rate;
-        if (fromCurrency == "VND")
+        if (fromCurrency === "VND")
             {
-                if (toCurrency == "VND") {rate = 1;}
-                else if (toCurrency == "USD") {rate = 0.2;}
-                else {rate = 0.4;}
+                if (toCurrency === "VND") rate = 1;
+                else if (toCurrency === "USD") rate = 0.2;
+                else rate = 0.4;
             }
-         else if (fromCurrency == "USD")
+        else if (fromCurrency === "USD")
             {
-                if (toCurrency == "VND") {rate = 5;}
-                else if (toCurrency == "USD") {rate = 1;}
-                else {rate = 2;}
+                if (toCurrency === "VND") rate = 5;
+                else if (toCurrency === "USD") rate = 1;
+                else rate = 2;
             }
         else
             {
-                if (toCurrency == "VND") {rate = 2.5;}
-                else if (toCurrency == "USD") {rate = 0.5;}
-                else {rate = 1;}
+                if (toCurrency === "VND") rate = 2.5;
+                else if (toCurrency === "USD") rate = 0.5;
+                else rate = 1;
             }
         let output = input * rate
         document.getElementById('result').innerText = "Result: " + output + toCurrency
